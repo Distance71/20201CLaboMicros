0b39d947a5398cfcd6158c43d623f0b2e7a0bdb0
Agus <agusdaverede@yahoo.com.ar>
2020-06-08 21:41:34 -0300

    Tp modificado

--- Tp2/Tp2/Tp2.asm
+++ Tp2/Tp2/Tp2.asm
@@ -9,16 +9,19 @@
 
 .ORG 0x0000
 
+;use of 
+
 start:
         CBI     DDRB,0	;PB0, input
-		SBI		DDRB,1	;PB1, output
+	CBI	PORTB,0 ;enables pull up at PORTB0
+	SBI	DDRB,1	;PB1, output
 
-main:	CBI		PORTB,1	;led turns off
-		SBIC	PINB,0	;is not pressed
-		CALL	turnOn
-		JMP		main
+main:	CBI	PORTB,1	;led turns off
+	SBIC	PINB,0	;is not pressed
+	CALL	turnOn
+	JMP	main
 
-turnOn:	SBI		PORTB,1
-		SBIC	PINB,0 ;is not pressed
-		JMP		turnOn
-		RET
+turnOn:	SBI	PORTB,1
+	SBIC	PINB,0 ;is not pressed
+	JMP		turnOn
+	RET
